c31c9f550845cadbc5d2f02e538bf3c24f635079
craz0r <craz0r@users.sourceforge.net>
2009-03-08 20:36:38 +0000

    Got rid of old firmware/ tree
    git-svn-id: https://welecw2000a.svn.sourceforge.net/svnroot/welecw2000a@49 5642ed1f-f172-45da-a169-216d43dc357c

--- firmware/trunk/FPGA/DownSampler/simFastAverage/sim.bat
+++
@@ -1,8 +0,0 @@
-ghdl -a ../Global-p.vhd
-ghdl -a ../Octave/FastFirCoeff-p.vhd
-ghdl -a ../PolyphaseDecimator-p.vhd
-ghdl -a ../FastAverage-ea.vhd
-ghdl -a ../Octave/InputValues-p.vhd
-ghdl -a ../TestbenchFastAverage-ea.vhd
-del test.log
-ghdl -r Testbench --vcd=test.vcd 2> test.log
--- firmware/trunk/FPGA/DownSampler/simFastPolyPhaseDecimator/sim.bat
+++
@@ -1,9 +0,0 @@
-ghdl -a ../Global-p.vhd
-ghdl -a ../Octave/FastFirCoeff-p.vhd
-ghdl -a ../PolyphaseDecimator-p.vhd
-ghdl -a ../FastPolyPhaseDecimator-ea.vhd
-ghdl -a ../TopFastPolyPhaseDecimator-ea.vhd
-ghdl -a ../Octave/InputValues-p.vhd
-ghdl -a ../TestbenchFastPolyphaseDecimator-ea.vhd
-del test.log
-ghdl -r Testbench --vcd=test.vcd 2> test.log
